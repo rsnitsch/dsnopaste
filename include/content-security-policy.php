aeeb8085ab85742c83f2d400c346a28e48200a5a
Robert Nitsch <r.s.nitsch@gmail.com>
2014-08-20 22:35:45 +0200

    Fix Piwik optout frame on imprint page (allow iframe in content-security-policy)

--- include/content-security-policy.php
+++ include/content-security-policy.php
@@ -1,5 +1,5 @@
 <?php
-	$csp_rules = "default-src 'none'; script-src 'self' anal.robertnitsch.de; style-src 'self'; img-src 'self'";
+	$csp_rules = "default-src 'none'; script-src 'self' anal.robertnitsch.de; style-src 'self'; img-src 'self'; frame-src anal.robertnitsch.de; child-src anal.robertnitsch.de";
 
 	foreach (array("X-WebKit-CSP", "X-Content-Security-Policy", "Content-Security-Policy") as $csp)
 	{
