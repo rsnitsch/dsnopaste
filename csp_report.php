22714b88b28cb8de8a2f3b793d00b3d5d7887f2d
Robert Nitsch <r.s.nitsch@gmail.com>
2014-08-21 09:22:22 +0200

    Improve formatting of csp_report log

--- csp_report.php
+++ csp_report.php
@@ -7,4 +7,4 @@
 	$c = json_decode($c, true);
 	$c = print_r($c, true);
 
-	file_put_contents("data/log/csp_errors.log", $c, FILE_APPEND);
+	file_put_contents("data/log/csp_errors.log", "Date: ".date("Y-m-d H:i:s")."\nReport:\n".$c."\n", FILE_APPEND);
