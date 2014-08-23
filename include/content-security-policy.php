784ec06bce967d32f858e78f691faeccd9d852a1
Robert Nitsch <r.s.nitsch@gmail.com>
2014-08-23 10:13:52 +0200

    Allow 'unsafe-eval' in CSP for piwik.js to function properly

--- include/content-security-policy.php
+++ include/content-security-policy.php
@@ -1,5 +1,5 @@
 <?php
-	$csp_rules = "default-src 'none'; script-src 'self' anal.robertnitsch.de; style-src 'self'; img-src 'self' anal.robertnitsch.de; frame-src anal.robertnitsch.de; child-src anal.robertnitsch.de; report-uri ".server_url()."/csp_report.php";
+	$csp_rules = "default-src 'none'; script-src 'self' 'unsafe-eval' anal.robertnitsch.de; style-src 'self'; img-src 'self' anal.robertnitsch.de; frame-src anal.robertnitsch.de; child-src anal.robertnitsch.de; report-uri ".server_url()."/csp_report.php";
 
 	foreach (array("X-WebKit-CSP", "X-Content-Security-Policy", "Content-Security-Policy") as $csp)
 	{
